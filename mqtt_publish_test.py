36a4f5ef99d79ad3ed30e97eb93d70bc865983a3
Matyi@ProBook <matyasszanto@gmail.com>
2022-06-25 20:08:43 +0200

    Publishing cleaned.

--- mqtt_publish_test.py
+++ mqtt_publish_test.py
@@ -22,8 +22,6 @@ client = mqtt_turmu.connect_mqtt(broker,
                                  keyfile_path
                                  )
 
-client.loop_start()
-time.sleep(1)
 mo.turmu_offline_mode_publish(client=client, number_of_objects=3, topic=topic)
 client.disconnect()
 
