3ce2cddaadf7f3bbef848c2564437b6bd8dd1014
Matyi@ProBook <matyasszanto@gmail.com>
2022-06-27 15:02:31 +0200

    refactor

--- mqtt_publish_test.py
+++ mqtt_publish_test.py
@@ -6,23 +6,27 @@ import time
 import mqtt_turmu
 import map_obstacle as mo
 
-# parameters for Tecnalia MQTT broker
-broker, port, client_id, username, password, ca_certs_path, certfile_path, keyfile_path = \
-    mqtt_turmu.load_default_params()
 
-topic = "testtopic/matyas"
+def run():
+    # parameters for Tecnalia MQTT broker
+    broker, port, client_id, username, password, ca_certs_path, certfile_path, keyfile_path = \
+        mqtt_turmu.load_default_params()
 
-client = mqtt_turmu.connect_mqtt(broker,
-                                 port,
-                                 client_id,
-                                 username,
-                                 password,
-                                 ca_certs_path,
-                                 certfile_path,
-                                 keyfile_path
-                                 )
+    topic = "testtopic/matyas"
 
-mo.turmu_offline_mode_publish(client=client, topic=topic, number_of_obstacles=3)
-client.disconnect()
+    client = mqtt_turmu.connect_mqtt(broker,
+                                     port,
+                                     client_id,
+                                     username,
+                                     password,
+                                     ca_certs_path,
+                                     certfile_path,
+                                     keyfile_path
+                                     )
+
+    mo.turmu_offline_mode_publish(client=client, topic=topic, number_of_obstacles=3)
+    client.disconnect()
 
 
+if __name__ == "__main__":
+    run()
