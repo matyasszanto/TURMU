25b6abde4fed9e416431d797ef9706bb1e994a41
Mátyás Szántó <51972157+matyasszanto@users.noreply.github.com>
2022-08-26 16:54:06 +0200

    publish_cycle_test.py now publishes real_test.json

--- publish_cycle_test.py
+++ publish_cycle_test.py
@@ -1,12 +1,38 @@
 import time
+import json
 
-import mqtt_publish_ego_test
-import mqtt_publish_test
+import map_obstacle as mo
+import mqtt_turmu
+import show_obstacles
 
 if __name__ == "__main__":
 
-    mqtt_publish_test.run(number_of_obstacles=2)
+    broker, port, client_id, username, password, ca_certs_path, certfile_path, keyfile_path = \
+        mqtt_turmu.load_default_params()
 
-    time.sleep(0.4)
+    topic = "testtopic/matyas"
+    topic = "iotac/Twizy-1/obstacles"
+    topic = "iotac/VirtualVehicle-1/obstacles"
+    # topic = "iotac/planner"
 
-    mqtt_publish_ego_test.run()
+    client = mqtt_turmu.connect_mqtt(broker,
+                                     port,
+                                     client_id,
+                                     username,
+                                     password,
+                                     ca_certs_path,
+                                     certfile_path,
+                                     keyfile_path
+                                     )
+
+    f = open("real_test.json")
+    full_run_json = json.load(f)
+    for publication in full_run_json:
+        # Obsts = []
+        # for obst in publication["obstacles"]:
+        #     Obst = mo.obstacle_object_from_mqtt_payload_obstacle_as_dict(obst)
+        #     Obsts.append(Obst)
+        #
+        # show_obstacles.plot_obstacles(Obsts)
+        client.publish(topic, json.dumps(publication))
+        time.sleep(0.3)
