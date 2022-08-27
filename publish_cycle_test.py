f7548af7232f897d283e1639cf317cd214ff0f00
Matyi@ProBook <matyasszanto@gmail.com>
2022-08-28 00:06:12 +0200

    obstacle visualization introduced

--- publish_cycle_test.py
+++ publish_cycle_test.py
@@ -3,15 +3,17 @@ import json
 
 import map_obstacle as mo
 import mqtt_turmu
-import show_obstacles
+import visualize_obstacles
+
+import numpy as np
 
 if __name__ == "__main__":
 
     broker, port, client_id, username, password, ca_certs_path, certfile_path, keyfile_path = \
         mqtt_turmu.load_default_params()
 
-    topic = "testtopic/matyas"
-    topic = "iotac/Twizy-1/obstacles"
+    # topic = "testtopic/matyas"
+    # topic = "iotac/Twizy-1/obstacles"
     topic = "iotac/VirtualVehicle-1/obstacles"
     # topic = "iotac/planner"
 
@@ -27,12 +29,53 @@ if __name__ == "__main__":
 
     f = open("real_test.json")
     full_run_json = json.load(f)
-    for publication in full_run_json:
-        # Obsts = []
-        # for obst in publication["obstacles"]:
-        #     Obst = mo.obstacle_object_from_mqtt_payload_obstacle_as_dict(obst)
-        #     Obsts.append(Obst)
-        #
-        # show_obstacles.plot_obstacles(Obsts)
+
+    plot = False
+    if plot:
+        max_step_map = 3000
+        max_obstacles_map = 50
+
+        max_step = (max_step_map if max_step_map < len(full_run_json) else len(full_run_json))
+
+        lats, longs = np.array([]), np.array([])
+        for publication in full_run_json[:max_step_map]:
+            max_obstacles = max_obstacles_map if max_obstacles_map < len(
+                publication["obstacles"]
+            ) else len(
+                publication["obstacles"]
+            )
+
+            obsts = publication["obstacles"][:max_obstacles_map]
+            obst_lats = list(o["latitude"] for o in obsts)
+            obst_longs = list(o["longitude"] for o in obsts)
+            lats = np.append(lats, [lat for lat in obst_lats])
+            longs = np.append(longs, [long for long in obst_longs])
+
+        lat_min = np.min(lats)
+        lat_max = np.max(lats)
+        long_min = np.min(longs)
+        long_max = np.max(longs)
+
+    max_step = 30
+    max_obstacles = 60
+
+    i = 0
+    for publication in full_run_json[:max_step]:
+        i += 1
+        if plot:
+            max_obstacles = max_obstacles if max_obstacles < len(publication["obstacles"]
+                                                                 ) else len(publication["obstacles"]
+                                                                            )
+            obsts = publication["obstacles"][:max_obstacles]
+            Obsts = []
+            for obst in obsts:
+                Obsts.append(mo.obstacle_object_from_mqtt_payload_obstacle_as_dict(obst))
+
+            visualize_obstacles.plot_obstacles(Obsts, i, lat_extremes=[lat_min, lat_max], long_extremes=[long_min, long_max])
+        else:
+            time.sleep(0.3)
+
+        # mqtt_turmu.publish_obstacles(client, topic, Obsts)
+        publication["obstacles"] = publication["obstacles"][:max_obstacles]
         client.publish(topic, json.dumps(publication))
-        time.sleep(0.3)
+        print(f"Publication: {i}/{max_step}")
