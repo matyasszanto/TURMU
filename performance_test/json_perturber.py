349b0bd48eedf68df8423a2d6329ba9de4975850
Matyi@ProBook <matyasszanto@gmail.com>
2023-01-14 17:37:11 +0100

    Refactor

--- performance_test/json_perturber.py
+++ performance_test/json_perturber.py
@@ -1,9 +1,8 @@
 import numpy as np
 import json
 
-with open("perf_test.json", "r") as file:
+with open("perf_test_original.json", "r") as file:
     perf_test = json.load(file)
-    print(perf_test)
 
 for pub in perf_test:
     obsts = pub['obstacles']
