27b8d1e005a96bd37a7e40fd8e35bbb179bd16b1
Matyi@ProBook <matyasszanto@gmail.com>
2023-01-14 17:49:14 +0100

    Order of magnitude for perturbation fixed.

--- performance_test/json_perturber.py
+++ performance_test/json_perturber.py
@@ -7,8 +7,8 @@ with open("perf_test_original.json", "r") as file:
 for pub in perf_test:
     obsts = pub['obstacles']
     for obst in obsts:
-        obst['latitude'] = obst['latitude'] + np.random.randn()*1e-5
-        obst['longitude'] = obst['longitude'] + np.random.randn()*1e-5
+        obst['latitude'] = obst['latitude'] + np.random.randn()*1e-6
+        obst['longitude'] = obst['longitude'] + np.random.randn()*1e-6
 
 perf_test_string = json.dumps(perf_test, indent=4)
 with open("perf_test.json", "w") as file:
